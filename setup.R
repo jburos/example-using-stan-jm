824a23da5f9ca863f7e3376a050a8756b7a0db6e
Jacki Novik <jackinovik@gmail.com>
2017-05-05 03:13:28 +0000

    use different development repo for stan_jm code

--- setup.R
+++ setup.R
@@ -8,5 +8,6 @@ install.packages('survminer')
 install.packages('stringr') ## without this, docopt install sometimes fails
 install.packages('docopt')
 install.packages('DT')
-devtools::install_github('sambrilleman/rstanarm', ref = 'develop2', args = '--preclean', local = TRUE)
+# devtools::install_github('sambrilleman/rstanarm', ref = 'develop2', args = '--preclean', local = TRUE)
+devtools::install_github('jburos/rstanarm', ref = 'fix-posterior-predict-newdata',  args = '--preclean', local = TRUE)
 install.packages('JM')
