097f28d3608fe40773d8e172e8a8fc1c013dc308
Jacki Novik <jackinovik@gmail.com>
2017-05-12 15:39:37 +0000

    function to evaluate submitted fits

--- inspect_cache_files.function.R
+++ inspect_cache_files.function.R
@@ -11,7 +11,7 @@ get_cache_files <- function(cache_dir = CACHE_DIR,
                             filetypes = c('stan_jm_fit'),
                             max_treedepth = 15,
                             n = c(200, 500),
-                            iter = c(2000, 5000),
+                            iter = c(2000, 3000, 5000, 8000),
                             ...) {
   argg <- c(as.list(environment()), list(...))
   argg[['cache_dir']] <- NULL
@@ -38,6 +38,49 @@ get_cache_files <- function(cache_dir = CACHE_DIR,
     unique() %>%
     unlist()
 
+}
+
+summarize_cached_result <- function(stanfit_cache, cache_dir = CACHE_DIR) {
+  
+  simdata_cache <- stringr::str_replace(stanfit_cache, pattern = 'stan_jm_fit', replacement = 'simulated_data')
+  pscheck_cache <- stringr::str_replace(stanfit_cache, pattern = 'stan_jm_fit', replacement = 'stan_jm_ps_check')
+  
+  # load data
+  simdat <- readRDS(file.path(cache_dir, simdata_cache))
+
+  dataLong <- simdat %>% 
+    dplyr::select(id, Z1, Z2, tij, starts_with('Yij'))
+  
+  dataEvent <- simdat %>%
+    dplyr::select(id, Z1, Z2, event, eventtime) %>%
+    dplyr::distinct(id, .keep_all = TRUE)
+  
+  # load stan_jm_fit
+  stan_jm_fit <- readRDS(file.path(cache_dir, stanfit_cache))
+  
+  # ps_check
+  ps_check <- readRDS(file.path(cache_dir, pscheck_cache))
+
+  # recover params
+  recovery_plot <- function() {
+    trueparams <- c('betaLong_binary', 'betaLong_continuous', 'betaLong_slope', 'betaEvent_assoc')
+    params <- c('Long1|Z1', 'Long1|Z2', 'Long1|tij', 'Assoc|Long1|etavalue')
+    
+    paramvals <- as.array(stan_jm_fit)[, , params, drop = FALSE]
+    truevals <- as.numeric(attr(simdat, 'params')[trueparams])
+    bayesplot::mcmc_recover_hist(paramvals, truevals, facet_args = list(scales = 'free', ncol = 1))
+  }
+  
+  rstanarm:::nlist(stan_jm_fit,
+                   simdat = simdat,
+                   ps_check = ps_check,
+                   recovery_plot = recovery_plot,
+                   dataLong = dataLong,
+                   dataEvent = dataEvent,
+                   beta_assoc = as.numeric(attr(simdat, 'params')['betaEvent_assoc']),
+                   beta_binary = as.numeric(attr(simdat, 'params')['betaLong_binary']),
+                   n = nrow(dataEvent)
+                   )
 }
                             
                             
